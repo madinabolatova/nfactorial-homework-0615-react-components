0fbfd0596dcecb87481a9d777f78ed36272c8546
madinabolatova <madinasabitkyzy25@gmail.com>
2022-06-23 23:17:28 +0600

    finished 3rd step

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { DefaultContext } from "./Context";
+
+//импортируем стили
+import "./App.css";
+
+import { Header } from "./components/header";
+import { Content } from "./components/content";
+import { Footer } from "./components/footer";
+
+export default function App() {
+  const [fan, setFan] = useState("");
+  const [theme, setTheme] = useState("night");
+
+  const handleCreateFan = ({ name }) => {
+    setFan(name);
+  };
+
+  const handleSetTheme = ({theme}) => {
+    if (theme === "night") {
+      setTheme("day");
+    } else {
+      setTheme("night");
+    }
+  };
+
+  return (
+    <DefaultContext.Provider value={{ handleCreateFan, handleSetTheme }}>
+      <Header fan={fan} theme={theme}/>
+      <hr />
+      <Content theme={theme}/>
+      <Footer theme={theme}/>
+
+    </DefaultContext.Provider>
+  );
+}
---
+++ src/components/footer/index.jsx
@@ -0,0 +1,21 @@
+export const Footer = ({theme}) => {
+    return (
+      <footer className={`${theme}`}>
+        <h4> May the force be with you</h4>
+        <h5>List of movies:</h5>
+        <div className="footer-links">
+            <a href="https://www.starwars.com/films/star-wars-episode-ix-the-rise-of-skywalker">STAR WARS: THE RISE OF SKYWALKER</a>
+            <a href="https://www.starwars.com/films/solo">SOLO: A STAR WARS STORY</a>
+            <a href="https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi">STAR WARS: THE LAST JEDI</a>
+            <a href="https://www.starwars.com/films/rogue-one">ROGUE ONE: A STAR WARS STORY</a>
+            <a href="https://www.starwars.com/films/star-wars-episode-vii-the-force-awakens">STAR WARS: THE FORCE AWAKENS</a>
+            <a href="https://www.starwars.com/films/star-wars-episode-vi-return-of-the-jedi">STAR WARS: RETURN OF THE JEDI</a>
+            <a href="https://www.starwars.com/films/star-wars-episode-v-the-empire-strikes-back">STAR WARS: THE EMPIRE STRIKES BACK</a>
+            <a href="https://www.starwars.com/films/star-wars-episode-iv-a-new-hope">STAR WARS: A NEW HOPE</a>
+            <a href="starwars.com/films/star-wars-episode-iii-revenge-of-the-sith">STAR WARS: REVENGE OF THE SITH</a>
+            <a href="starwars.com/films/star-wars-episode-ii-attack-of-the-clones">STAR WARS: ATTACK OF THE CLONES</a>
+            <a href="starwars.com/films/star-wars-episode-i-the-phantom-menace">STAR WARS: THE PHANTOM MENACE</a>
+        </div>
+      </footer>
+    );
+  };
